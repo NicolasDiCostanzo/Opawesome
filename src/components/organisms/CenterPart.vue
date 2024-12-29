34986c720b61b4a2e8e7eb673cecc2dfadf4a0e2
Nicolas Di Costanzo <dicostanzo.nicolas@gmail.com>
2024-12-29 16:43:41 +0100

    refactor: move loadFontsAndAddTextbox to canvas helper

--- src/components/organisms/CenterPart.vue
+++ src/components/organisms/CenterPart.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { defineProps, onMounted, watch } from 'vue';
 import { fabric } from 'fabric';
-import { loadImageToCanvas } from '@/helper/canvas-helper';
+import { loadImageToCanvas, loadFontsAndAddTextbox } from '@/helper/canvas-helper';
 const props = defineProps({
   selectedImageUrl: {
     type: String,
@@ -14,43 +14,16 @@ let canvas;
 onMounted(() => {
   canvas = new fabric.Canvas("canvas");
   loadImageToCanvas(props.selectedImageUrl, canvas);
-  loadFontsAndAddTextbox();
+  loadFontsAndAddTextbox(canvas);
 });
 
 watch(() => props.selectedImageUrl, (newUrl) => {
   loadImageToCanvas(newUrl, canvas);
-  loadFontsAndAddTextbox();
+  loadFontsAndAddTextbox(canvas);
 });
 
 
-function loadFontsAndAddTextbox() {
-  const urlMap = {
-    VT323: 'url(https://fonts.gstatic.com/s/vt323/v17/pxiKyp0ihIEF2isfFJXUdVNF.woff2)',
-    Pacifico: 'url(https://fonts.gstatic.com/s/pacifico/v22/FwZY7-Qmy14u9lezJ-6H6MmBp0u-.woff2)',
-    Lato100: 'url(https://fonts.gstatic.com/s/lato/v24/S6u8w4BMUTPHh30AXC-qNiXg7Q.woff2)',
-    Lato900: 'url(https://fonts.gstatic.com/s/lato/v24/S6u9w4BMUTPHh50XSwiPGQ3q5d0.woff2)',
-  };
 
-  const fontVT323 = new FontFace('VT323', urlMap.VT323, {
-    style: 'normal',
-    weight: 'normal',
-  });
-
-  Promise.all([fontVT323.load()]).then(() => {
-    document.fonts.add(fontVT323);
-
-    const vtTextBox = new fabric.Textbox('test ptn', {
-      left: 50,
-      top: 50,
-      width: 200,
-      fontSize: 60,
-      fontFamily: 'VT323',
-      fill: 'black'
-    });
-
-    canvas.add(vtTextBox);
-  });
-}
 </script>
 
 <template>
--- src/helper/canvas-helper.js
+++ src/helper/canvas-helper.js
@@ -33,14 +33,13 @@ export function resizeCanvas(imgWidth, imgHeight) {
 }
 
 export function loadImageToCanvas(url, canvas) {
+  canvas.clear();
+  
   fabric.Image.fromURL(url, (img) => {
-    console.log("Image loaded:", img);
-
     const { canvasWidth, canvasHeight} = resizeCanvas(img.width, img.height)
     canvas.setWidth(canvasWidth);
     canvas.setHeight(canvasHeight);
 
-    canvas.clear();
 
     const scaleX = canvasWidth / img.width;
     const scaleY = canvasHeight / img.height;
@@ -48,5 +47,52 @@ export function loadImageToCanvas(url, canvas) {
       scaleX,
       scaleY
     });
+  });
+}
+
+export function loadFontsAndAddTextbox(canvas) {
+  canvas.clear();
+  const urlMap = {
+    VT323: 'url(https://fonts.gstatic.com/s/vt323/v17/pxiKyp0ihIEF2isfFJXUdVNF.woff2)',
+    Pacifico: 'url(https://fonts.gstatic.com/s/pacifico/v22/FwZY7-Qmy14u9lezJ-6H6MmBp0u-.woff2)',
+    Lato100: 'url(https://fonts.gstatic.com/s/lato/v24/S6u8w4BMUTPHh30AXC-qNiXg7Q.woff2)',
+    Lato900: 'url(https://fonts.gstatic.com/s/lato/v24/S6u9w4BMUTPHh50XSwiPGQ3q5d0.woff2)',
+  };
+
+  const fontVT323 = new FontFace('VT323', urlMap.VT323, {
+    style: 'normal',
+    weight: 'normal',
+  });
+
+  const fontPacifico = new FontFace('Pacifico', urlMap.Pacifico, {
+    style: 'normal',
+    weight: 'normal',
+  });
+
+  Promise.all([fontVT323.load(), fontPacifico.load()]).then(() => {
+    document.fonts.add(fontVT323);
+    document.fonts.add(fontPacifico);
+    const vtTextBox = new fabric.Textbox('test ptn', {
+      left: 50,
+      top: 50,
+      width: 200,
+      fontSize: 60,
+      fontFamily: 'VT323',
+      fill: 'black'
+    });
+
+    const pacificoTextBox = new fabric.Textbox('test ptn', {
+      left: 50,
+      top: 150,
+      width: 200,
+      fontSize: 60,
+      fontFamily: 'Pacifico',
+      fill: 'black'
+    });
+
+    canvas.add(vtTextBox, pacificoTextBox);
+    canvas.renderAll();
+  }).catch((error) => {
+    console.error("Error loading font:", error);
   });
 }
