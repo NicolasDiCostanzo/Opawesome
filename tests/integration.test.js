1460aac817219c168351cc68ce34475913df2e67
Nicolas Di Costanzo <dicostanzo.nicolas@gmail.com>
2025-01-05 13:58:58 +0100

    test: rename example.test.js to integration.test.js and refactor textbox retrieval logic

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -5,36 +5,30 @@ import {
 import { fabric } from 'fabric';
 import App from '../src/App.vue';
 
+function getTypeOfObjectsFromCanvas(canvas, typeOfObject) {
+  return canvas.getObjects().filter((obj) => obj instanceof typeOfObject);
+}
+
 describe('App.vue', () => {
   it('spawns a textbox on the CentralPage component when the "Add" button of the LeftPart component is clicked', async () => {
-    // Create a mock canvas object
     const mockCanvas = new fabric.Canvas();
     mockCanvas.add = vi.fn(function add(obj) {
       this._objects.push(obj);
     });
-    mockCanvas.setActiveObject = vi.fn();
-    mockCanvas.renderAll = vi.fn();
 
-    // Mount the App component
     const wrapper = mount(App);
-
-    // Trigger the updateCanvas method to set the canvas
     wrapper.vm.updateCanvas(mockCanvas);
     await wrapper.vm.$nextTick();
 
-    // Find the LeftPart component
     const leftPart = wrapper.findComponent({ name: 'LeftPart' });
 
-    // Find the "Add" button and click it
     const addButton = leftPart.find('button');
     await addButton.trigger('click');
 
-    // Check the objects on the canvas
-    const objects = mockCanvas.getObjects();
-    const textboxes = objects.filter((obj) => obj instanceof fabric.Textbox);
+    const textboxes = getTypeOfObjectsFromCanvas(mockCanvas, fabric.Textbox);
     expect(textboxes.length).toBe(1);
+
     const foundTextBox = textboxes[0];
-    expect(foundTextBox.text).toBe('this font');
     expect(foundTextBox.fontFamily).toBe('Arial');
   });
 });
