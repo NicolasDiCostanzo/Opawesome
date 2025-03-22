67a45c24b073ed26c01a064a80d3bc63964d4148
Nicolas Di Costanzo <dicostanzo.nicolas@gmail.com>
2025-03-22 11:17:41 +0100

    style: improve layout and responsiveness of RightPart component

--- src/components/organisms/RightPart.vue
+++ src/components/organisms/RightPart.vue
@@ -13,58 +13,72 @@ function selectImage(imageUrl) {
 </script>
 
 <template>
-    <div class="right-part">
-      <div id="imagesContainer">
-        <ul>
-          <li v-for="image in images" :key="image.id">
-            <img :class="selectedImageUrl === image.url ? 'selected' : ''" :src=image.url @click="selectImage(image.url)" alt='cute opossum option'/>
-          </li>
-        </ul>
-      </div>
+  <div class="right-part">
+    <div id="imagesContainer">
+      <ul>
+        <li v-for="image in images" :key="image.id">
+          <img :class="selectedImageUrl === image.url ? 'selected' : ''" :src=image.url @click="selectImage(image.url)"
+            alt='cute opossum option' />
+        </li>
+      </ul>
     </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-@use '../../constants/style/constants.scss' as *;
+  @use '../../constants/style/constants.scss' as *;
 
-$space-around-images: 15px;
+  $space-around-images: 15px;
 
-ul {
+  ul {
     overflow-y: auto;
     display: grid;
-      justify-content: space-evenly;
+    justify-content: space-evenly;
     grid-template-columns: repeat(2, $IMAGES_SIZE);
-    gap:$space-around-images;
-    padding:$space-around-images;
+    gap: $space-around-images;
+    padding: $space-around-images;
     align-content: start;
-}
 
-li {
-  all: unset;
-}
+    @media (max-width: 768px) {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: start;
+      align-self: center;
+      overflow-y: auto;
+      max-height: 250px;
+    }
+  }
 
-.right-part {
+  li {
+    all: unset;
+  }
+
+  .right-part {
     display: flex;
     align-items: center;
     flex-direction: column;
     justify-content: space-evenly;
-}
 
-img {
+    @media (max-width: 768px) {
+      justify-content: start;
+    }
+  }
+
+  img {
     max-width: $IMAGES_SIZE;
     max-height: $IMAGES_SIZE;
     transition: transform 0.25s ease, box-shadow 0.1s ease;
     box-shadow: rgba(0, 0, 0, 0.45) 0px 0px 15px;
-}
+  }
 
-img:hover {
+  img:hover {
     cursor: pointer;
     box-shadow: rgba(0, 0, 0, 1) 0px 0px 15px;
     transform: rotate(3.5deg); // maybe random ?
-}
+  }
 
-img.selected {
+  img.selected {
     box-shadow: 0 0 0 2px var(--tea-rose-red);
     box-sizing: border-box;
-}
+  }
 </style>
