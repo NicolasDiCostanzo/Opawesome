0877210b766fd690cf6c66306d25554564b02fa9
Nicolas Di Costanzo <dicostanzo.nicolas@gmail.com>
2025-08-16 13:21:30 +0200

    fix: add no-trailing-spaces rule configuration for improved code quality

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,17 @@ module.exports = {
     'max-len': 'off',
     'class-methods-use-this': 'off',
     'import/no-extraneous-dependencies': 'off',
+    'no-trailing-spaces': ['error', {
+      skipBlankLines: true,
+      ignoreComments: true,
+    }],
   },
+  overrides: [
+    {
+      files: ['*.vue'],
+      rules: {
+        'no-trailing-spaces': 'off',
+      },
+    },
+  ],
 };
